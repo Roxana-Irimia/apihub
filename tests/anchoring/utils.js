729ce8b13090d71281d6c76e32f91fedfea40648
skutner <sava.dumitru.daniel@gmail.com>
2022-11-07 17:23:13 +0200

    PharmaLedger-IMI/epi-workspace#924 hint should be a json

--- tests/anchoring/utils.js
+++ tests/anchoring/utils.js
@@ -1,30 +1,30 @@
 let keySSIApis = require("opendsu").loadAPI("keyssi");
 
-function generateSeedSSI(){
+function generateSeedSSI() {
     const domain = 'default';
     return keySSIApis.createSeedSSI(domain);
 }
 
-function generateConstSSI(){
+function generateConstSSI() {
     const domain = 'default';
     return keySSIApis.createConstSSI(domain);
 }
 
-async function getAnchorId(seedSSI){
+async function getAnchorId(seedSSI) {
     return await $$.promisify(seedSSI.getAnchorId)();
 }
 
-async function getSignedHashLink(seedSSI, previousSignHashLinkId){
+async function getSignedHashLink(seedSSI, previousSignHashLinkId) {
     const domain = 'default';
     let anchorSSI = keySSIApis.parse(await getAnchorId(seedSSI));
     let previousSignHashLinkSSI = null;
-    if (previousSignHashLinkId){
+    if (previousSignHashLinkId) {
         previousSignHashLinkSSI = keySSIApis.parse(previousSignHashLinkId);
     }
     const timestamp = Date.now();
 
     const dummy = keySSIApis.createSignedHashLinkSSI(domain, "HASH1", timestamp, "signature", seedSSI.getVn());
-    let dataToSign = dummy.getDataToSign(anchorSSI,previousSignHashLinkSSI);
+    let dataToSign = dummy.getDataToSign(anchorSSI, previousSignHashLinkSSI);
 
     let signature = await $$.promisify(seedSSI.sign)(dataToSign);
     const signedHashLinkSSI = keySSIApis.createSignedHashLinkSSI(domain, "HASH1", timestamp, signature, seedSSI.getVn());
@@ -32,11 +32,10 @@ async function getSignedHashLink(seedSSI, previousSignHashLinkId){
 }
 
 
-function getHashLink(constSSI){
+function getHashLink(constSSI) {
     const domain = 'default';
-    return keySSIApis.createHashLinkSSI(domain,'some hash data', constSSI.getVn(),'hint').getIdentifier();
+    return keySSIApis.createHashLinkSSI(domain, 'some hash data', constSSI.getVn()).getIdentifier();
 }
-
 
 
 module.exports = {
