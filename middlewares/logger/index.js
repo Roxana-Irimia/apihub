9686740730b6d4c4f3a63b88eaab9ee3924c714f
skutner <sava.dumitru.daniel@gmail.com>
2022-11-15 15:36:40 +0200

    cleanup

--- middlewares/logger/index.js
+++ middlewares/logger/index.js
@@ -6,9 +6,6 @@ function Logger(server) {
         const diff = process.hrtime(start);
         return (diff[0] * 1e9 + diff[1]) / 1e6;
     };
-    
-  const { getConfig } = require("../../config");
-  const port = getConfig('port');
 
   server.use(function (req, res, next) {
     const {
